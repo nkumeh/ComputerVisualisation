8d7fe415299dd365e259916a623983a6186154ea
Kristine Nnemka <74774510+nkumeh@users.noreply.github.com>
2023-03-02 22:08:41 -0800

    Add files via upload

---
+++ Assignment1ALT/vidDisplay.hpp
@@ -0,0 +1,13 @@
+//
+//  vidDisplay.hpp
+//  image_filter
+//
+//  Created by Kristine  Umeh on 1/22/23.
+//
+
+#ifndef vidDisplay_hpp
+#define vidDisplay_hpp
+
+#include <stdio.h>
+
+#endif /* vidDisplay_hpp */
